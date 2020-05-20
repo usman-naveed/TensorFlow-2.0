d9ed3d85eadd7f9811e961d958fca0b7ba69ec4e
Usman <mohammadusman94@hotmail.ca>
2020-05-20 15:55:41 -0400

    Completed Color CNN implementation

--- CNN/Color_CNN.py
+++ CNN/Color_CNN.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import os
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
 import logging
 
@@ -28,4 +28,108 @@ num_dogs_tr = len(os.listdir(train_dogs))
 num_cats_vl = len(os.listdir(validation_cats))
 num_dogs_vl = len(os.listdir(validation_dogs))
 
-print("Number of Cats in the Training Set: {}\n Number of Dogs in the Training Set: {}\n Number of Cats in the Validation Set: {}\n Number of Dogs in the Validation Set: {}\n".format(num_cats_tr, num_dogs_tr, num_cats_vl, num_dogs_vl))
+total_train = num_cats_tr + num_dogs_tr
+total_val = num_cats_vl + num_dogs_vl
+
+print("Number of Cats in the Training Set: {}\n Number of Dogs in the Training Set: {}\n Number of Cats in the "
+      "Validation Set: {}\n Number of Dogs in the Validation Set: {}\n".format(num_cats_tr, num_dogs_tr, num_cats_vl,
+                                                                               num_dogs_vl))
+#Set some pre-processing variables
+BATCH_SIZE = 100
+IMG_SHAPE = 150
+
+'''
+Images must be formatted into appropriately pre-processed floating point tensors before being fed into the network. The steps involved in preparing these images are:
+
+Read images from the disk
+Decode contents of these images and convert it into proper grid format as per their RGB content
+Convert them into floating point tensors
+Rescale the tensors from values between 0 and 255 to values between 0 and 1, as neural networks prefer to deal with small input values.
+Fortunately, all these tasks can be done using the class tf.keras.preprocessing.image.ImageDataGenerator.
+
+We can set this up in a couple of lines of code below.
+'''
+train_image_generator = ImageDataGenerator(rescale=1./255)
+validation_image_generator = ImageDataGenerator(rescale= 1./255)
+
+train_data_gen = train_image_generator.flow_from_directory(batch_size=BATCH_SIZE,
+                                                           directory=training_dir,
+                                                           shuffle=True,
+                                                           target_size=(IMG_SHAPE,IMG_SHAPE), #(150,150)
+                                                           class_mode='binary')
+
+val_data_gen = validation_image_generator.flow_from_directory(batch_size=BATCH_SIZE,
+                                                              directory=validation_dir,
+                                                              shuffle=False,
+                                                              target_size=(IMG_SHAPE,IMG_SHAPE), #(150,150)
+                                                              class_mode='binary')
+
+
+sample_training_images, _ = next(train_data_gen)
+# This function will plot images in the form of a grid with 1 row and 5 columns where images are placed in each column.
+def plotImages(images_arr):
+    fig, axes = plt.subplots(1, 5, figsize=(20,20))
+    axes = axes.flatten()
+    for img, ax in zip(images_arr, axes):
+        ax.imshow(img)
+    plt.tight_layout()
+    plt.show()
+
+plotImages(sample_training_images[:5])  # Plot images 0-4
+
+#build the model
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+
+    tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+
+    tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(2)
+])
+
+#compile the model
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+#train the model
+EPOCHS = 3   #should be much higher for better results, but for the sake of computation time I have just set it to 3 Epochs
+history = model.fit_generator(
+    train_data_gen,
+    steps_per_epoch=int(np.ceil(total_train / float(BATCH_SIZE))),
+    epochs=EPOCHS,
+    validation_data=val_data_gen,
+    validation_steps=int(np.ceil(total_val / float(BATCH_SIZE)))
+)
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(EPOCHS)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.savefig('./foo.png')
+plt.show()
