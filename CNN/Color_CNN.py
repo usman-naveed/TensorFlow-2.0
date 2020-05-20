862bf14f5f6e51b22d46aa0c2910f7947acf8d18
usman-naveed <mohammadusman94@hotmail.ca>
2020-05-20 18:37:18 -0400

    add dropout to increase accuracy

--- CNN/Color_CNN.py
+++ CNN/Color_CNN.py
@@ -91,6 +91,7 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
     tf.keras.layers.MaxPooling2D(2, 2),
 
+    tf.keras.layers.Dropout(0.5),
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(512, activation='relu'),
     tf.keras.layers.Dense(2)
