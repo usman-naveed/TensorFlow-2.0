96f6bc55ec73b85e8a5d697095cae5f7696274ce
Usman <mohammadusman94@hotmail.ca>
2020-05-20 14:51:12 -0400

    Fixed logger error

--- CNN/Color_CNN.py
+++ CNN/Color_CNN.py
@@ -5,7 +5,7 @@ import matplotlib as plt
 import numpy as np
 import logging
 
-logger = tf.get_logger
+logger = tf.get_logger()
 logger.setLevel(logging.ERROR)
 
 #downloading the data from external URL, using tf.get_file()
@@ -20,3 +20,12 @@ train_cats = os.path.join(training_dir, 'cats')
 train_dogs = os.path.join(training_dir, 'dogs')
 validation_cats = os.path.join(validation_dir, 'cats')
 validation_dogs = os.path.join(validation_dir, 'dogs')
+
+#Explore the data
+num_cats_tr = len(os.listdir(train_cats))
+num_dogs_tr = len(os.listdir(train_dogs))
+
+num_cats_vl = len(os.listdir(validation_cats))
+num_dogs_vl = len(os.listdir(validation_dogs))
+
+print("Number of Cats in the Training Set: {}\n Number of Dogs in the Training Set: {}\n Number of Cats in the Validation Set: {}\n Number of Dogs in the Validation Set: {}\n".format(num_cats_tr, num_dogs_tr, num_cats_vl, num_dogs_vl))
